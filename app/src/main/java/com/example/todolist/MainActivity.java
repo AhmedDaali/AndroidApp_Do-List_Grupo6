f9d739e9cd72f18eaa26758ccb00b04d76d915dc
ahmed <ahmed_daali@hotmail.es>
2023-01-26 13:00:00 +0000

    Añadir los toasts, AlertDialog de las tareas y la funcionalidad del logout.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.todolist;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,9 +32,26 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.mas:
                 //activar el cuadro de dialogo para añadir tarea
+
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Nueva Tarea")
+                        .setMessage("¿Qué quieres hacer a continuación?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        //añadir tarea a la base de datos
+                                        //Toast.makeText(this, "Tarea añadida", Toast.LENGTH_LONG).show();
+                                    }
+                                })
+                        .setNegativeButton("Cancelar",null)
+                        .create();
+                dialog.show();
                 return true;
             case R.id.logout:
                 //Cierre de seión de Firebase
+                startActivity(new Intent(MainActivity.this,Login.class));
                 return true;
         }
         return super.onOptionsItemSelected(item);
