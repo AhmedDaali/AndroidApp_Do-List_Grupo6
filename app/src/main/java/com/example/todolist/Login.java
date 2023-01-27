7b4327adff4dd726243c8a8b527fb95793071efb
ahmed <ahmed_daali@hotmail.es>
2023-01-27 14:49:41 +0000

    Crear las tareas

--- app/src/main/java/com/example/todolist/Login.java
+++ app/src/main/java/com/example/todolist/Login.java
@@ -1,17 +1,26 @@
 package com.example.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     Button botonLogin;
     TextView botonRegistro;
+    EditText emailText,passwordText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,24 +28,66 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        emailText = findViewById(R.id.cajaCorreo);
+        passwordText = findViewById(R.id.cajaPassword);
+
         botonLogin = findViewById(R.id.botonLogin);
-        botonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this,MainActivity.class);
-                startActivity(intent);
-            }
+
+        botonLogin.setOnClickListener(view -> {
+
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Intent intent = new Intent(Login.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(Login.this, "Usuario no registrado.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
         });
 
         botonRegistro = findViewById(R.id.botonRegistro);
-        botonRegistro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        botonRegistro.setOnClickListener(view -> {
+
                 //Crear el usuario en Firebase
-                Toast.makeText(Login.this, "Usuario registrado", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( this,new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(Login.this, "Usuario registrado", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(Login.this, MainActivity.class);
+                                    startActivity(intent);
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Login.this, "No se ha podido crear al usuario.",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+
+
+            });
+        }
+
 }
